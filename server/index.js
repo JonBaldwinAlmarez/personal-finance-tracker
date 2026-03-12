1e28e27410b864e82ac1a773a0f4699069b9b80b
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-03-12 18:04:16 +0800

    Fix API_URL: add based url

--- server/index.js
+++ server/index.js
@@ -8,13 +8,11 @@ const app = express();
 // --- MIDDLEWARE ---
 app.use(
 	cors({
-		origin: [
-			"http://localhost:5173",
-			"advance-personal-finance-tracker.netlify.app",
-		],
+		origin: process.env.CORS_ORIGIN || "http://localhost:5173",
 		credentials: true,
 	}),
 ); // Enable CORS for all routes
+
 app.use(express.json()); // Middleware to parse JSON bodies
 
 // --- ROUTES ---
